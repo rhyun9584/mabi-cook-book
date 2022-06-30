82f62c008df387a62da799c3cdfe99ed0a49c341
rhyun9584 <rhyun9584@naver.com>
2022-07-01 00:08:35 +0900

    form validation을 실시간으로 할 수 있도록 변경
    jQuery validate 라이브러리 활용

--- app/forms.py
+++ app/forms.py
@@ -16,30 +16,27 @@ from wtforms.validators import (
 from app.models import User
 
 
-
 class UserCreateForm(FlaskForm):
-    email = EmailField('이메일 ID', validators=[DataRequired(), Email()])
-    password1 = PasswordField('비밀번호', validators=[
-        DataRequired(), EqualTo('password2', '비밀번호가 일치하지 않습니다.'), Length(min=6)])
-    password2 = PasswordField('비밀번호 확인', validators=[DataRequired()])
+    email = EmailField('이메일 ID')
+    password1 = PasswordField('비밀번호')
+    password2 = PasswordField('비밀번호 확인')
     server = SelectField('서버', choices=['류트', '하프', '울프', '만돌린'])
-    name = StringField('닉네임', validators=[DataRequired()])
+    name = StringField('닉네임')
 
 
 class UserSigninForm(FlaskForm):
-    email = EmailField('이메일 ID', validators=[DataRequired(), Email()])
-    password = PasswordField('비밀번호', validators=[DataRequired()])
+    email = EmailField('이메일 ID')
+    password = PasswordField('비밀번호')
 
 
 class ChangePwForm(FlaskForm):
-    old_pw = PasswordField('현재 비밀번호', validators=[DataRequired()])
-    new_pw1 = PasswordField('새 비밀번호', validators=[DataRequired(),
-                                                  EqualTo('new_pw2', '비밀번호가 일치하지 않습니다.'), Length(min=6)])
-    new_pw2 = PasswordField('새 비밀번호 확인', validators=[DataRequired()])
+    old_pw = PasswordField('현재 비밀번호')
+    new_pw1 = PasswordField('새 비밀번호')
+    new_pw2 = PasswordField('새 비밀번호 확인')
 
 
 class SearchPwForm(FlaskForm):
-    email = EmailField('이메일 ID', validators=[DataRequired(), Email()])
+    email = EmailField('이메일 ID')
 
     def validate_email(self, field):
         if User.query.filter_by(email=field.data).first() is None:
@@ -47,6 +44,5 @@ class SearchPwForm(FlaskForm):
 
 
 class ResetPwForm(FlaskForm):
-    new_pw1 = PasswordField('새 비밀번호', validators=[DataRequired(),
-                                                  EqualTo('new_pw2', '비밀번호가 일치하지 않습니다.'), Length(min=6)])
-    new_pw2 = PasswordField('새 비밀번호 확인', validators=[DataRequired()])
+    new_pw1 = PasswordField('새 비밀번호')
+    new_pw2 = PasswordField('새 비밀번호 확인')
---
+++ app/static/js/validate.js
@@ -0,0 +1,135 @@
+jQuery.validator.setDefaults({
+    errorElement: "div",
+    errorPlacement: function (error, element) {
+        error.addClass('invalid-feedback');
+        console.log(element.closest('.form-group'))
+        element.closest('div').append(error);
+    },
+})
+
+$("#signup-form").validate({
+    rules: {
+        email: {
+            required: true,
+            email: true,
+        },
+        password1: {
+            required: true,
+            minlength: 6,
+        },
+        password2: {
+            required: true,
+            equalTo: '#password1',
+        },
+        server: {
+            required: true,
+        },
+        name: {
+            required: true,
+        },
+    },
+    messages: {
+        email: {
+            required: "* 이메일 ID를 입력해주세요.",
+            email: "* 이메일 형식으로 입력해주세요."
+        },
+        password1: {
+            required: "* 비밀번호를 입력해주세요.",
+            minlength: "* 6자 이상 입력해주세요.",
+        },
+        password2: {
+            required: "* 비밀번호 확인을 입력해주세요.",
+            equalTo: "* 비밀번호가 일치하지 않습니다.",
+        },
+        server: {
+            required: "* 서버를 선택해주세요.",
+        },
+        name: {
+            required: "* 닉네임을 입력해주세요.",
+        },
+    },
+});
+
+$("#signin-form").validate({
+    rules: {
+        email: {
+            required: true,
+        },
+        password: {
+            required: true,
+        },
+    },
+    messages: {
+        email: {
+            required: "* 이메일 ID를 입력해주세요.",
+        },
+        password: {
+            required: "* 비밀번호를 입력해주세요.",
+        }
+    },
+});
+
+$("#changepw-form").validate({
+    rules: {
+        old_pw: {
+            required: true,
+        },
+        new_pw1: {
+            required: true,
+            minlength: 6,
+        },
+        new_pw2: {
+            required: true,
+            equalTo: "#new_pw1",
+        },
+    },
+    messages: {
+        old_pw: {
+            required: "* 현재 비밀번호를 입력해주세요.",
+        },
+        new_pw1: {
+            required: "* 새 비밀번호를 입력해주세요.",
+            minlength: "* 6자 이상 입력해주세요.",
+        },
+        new_pw2: {
+            required: "* 새 비밀번호 확인을 입력해주세요.",
+            equalTo: "* 새 비밀번호가 일치하지 않습니다.",
+        },
+    },
+});
+
+$("#searchpw-form").validate({
+    rules: {
+        email: {
+            required: true,
+        },
+    },
+    messages: {
+        email: {
+            required: "* 이메일 ID를 입력하세요.",
+        },
+    },
+});
+
+$("#resetpw-form").validate({
+    rules: {
+        new_pw1: {
+            required: true,
+            minlength: 6,
+        },
+        new_pw2: {
+            required: true,
+            equalTo: "#new_pw1",
+        },
+    },
+    messages: {
+        new_pw1: {
+            required: "* 새 비밀번호를 입력해주세요.",
+            minlength: "* 6자 이상 입력해주세요.",
+        },
+        new_pw2: {
+            required: "* 새 비밀번호 확인을 입력해주세요.",
+            equalTo: "* 새 비밀번호가 일치하지 않습니다.",
+        },
+    }
+});
