01aae5ec1f49bf22bedeaeffa3b4ef601d5eb098
rhyun9584 <rhyun9584@naver.com>
2022-10-17 22:34:18 +0900

    db import 스크립트가 dotenv로부터 db 정보를 받아오도록 수정

--- db_import/import.py
+++ db_import/import.py
@@ -1,7 +1,22 @@
+import os
 import pandas as pd
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
+
+
+load_dotenv()
+
+db = {
+    'user': os.environ.get("DB_USER"),
+    'password': os.environ.get("DB_PASSWORD"),
+    'host': os.environ.get("DB_HOST"),
+    'port': os.environ.get("DB_PORT"),
+    'database': os.environ.get("DB_DATABASE"),
+}
 
 table = pd.read_excel("./db_import/cook_list.xlsx", header=0, index_col=0)
 
-engine = create_engine("mysql+pymysql://mabi:1234@127.0.0.1:3306/mabi_cook", encoding='utf-8')
+engine = create_engine(f"mysql+pymysql://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}", \
+                       encoding='utf-8')
 table.to_sql(name="cook", con=engine, if_exists='append', index=False)
